a70257b3c5514384b0c6dacf6d2c8cf58c2f9ae0
anawat wannapila <knownamenow@gmail.com>
2022-05-15 01:33:53 +0700

    add index path

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const express = require("express");
-const fetch = require("node-fetch");
-const app = express();
+const express = require('express')
+const fetch = require('node-fetch')
+const app = express()
 
-app.get("/", async function (_, res) {
-  const response = await fetch("https://curlmyip.org");
-  const ip = await response.text();
-  const html = `<pre style="font-size: 20px;word-wrap: break-word; white-space: pre-wrap;">${ip}</pre>`;
-  res.send(html);
-});
+app.get('/', async function (_, res) {
+  res.send(`helloooooooo`)
+})
+
+app.get('/ip', async function (_, res) {
+  const response = await fetch('https://curlmyip.org')
+  const ip = await response.text()
+  const html = `<pre style="font-size: 20px;word-wrap: break-word; white-space: pre-wrap;">${ip}</pre>`
+  res.send(html)
+})
 
 app.listen(8080, () => {
-  console.log(`listening at http://localhost:${8080}`);
-});
+  console.log(`listening at http://localhost:${8080}`)
+})
